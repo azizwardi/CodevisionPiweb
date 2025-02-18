14fc8976ffb9947c2d74978b7ac8b5e7ec8c48c2
azizwardi <wardiaziz2507@gmail.com>
2025-02-19 00:11:39 +0100

    backend

---
+++ Backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT ;
+
+// Middleware
+app.use(express.json()); // Parses incoming JSON requests
+app.use(cors()); // Enables CORS
+
+// Sample Route
+app.get('/', (req, res) => {
+    res.send('Hello, Express!');
+});
+
+// Start the Server
+app.listen(PORT, () => {
+    console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
+
+// Connect to MongoDB
+mongoose
+  mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log('✅ MongoDB Connected Successfully'))
+  .catch((err) => console.error('❌ MongoDB Connection Error:', err));
+
+;
