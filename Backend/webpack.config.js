803d6aa58f6df3e8ed12b9f94a84c8bf3fe08d69
MalikOuni <122649655+MalikOuni@users.noreply.github.com>
2025-03-25 04:35:04 +0100

    Update webpack.config.js

--- Backend/webpack.config.js
+++ Backend/webpack.config.js
@@ -1,30 +1,33 @@
-const path = require("path");
+const path =require ('path');
 
-module.exports = {
-  // Point d'entrée de l'application
-  entry: "./src/index.js", // Si tu n'as pas de fichier `index.js`, remplace-le par ton fichier principal comme `server.js`
 
-  // Où Webpack va placer le fichier compilé
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
-  },
+module.exports={
 
-  // Résolution des extensions de fichier
-  resolve: {
-    extensions: [".js", ".json"], // Ajoute d'autres extensions si nécessaire
-  },
+    entry: {
+        main:'./server.js'
+    },
 
-  // Configuration pour les modules (par exemple, Babel pour la transpilation)
-  module: {
-    rules: [
-      {
-        test: /\.js$/, // Toutes les extensions .js
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader", // Utilise Babel pour transpiler ton code JS
-        },
-      },
-    ],
-  },
-};
+    output:{
+
+        path : path.join(__dirname,'dev_build'),
+
+        publicPath:'/',
+        filename: '[name].js',
+        clean:true
+    },
+
+    mode:'development',
+    target:'node',
+
+    module: {
+
+        rules: [
+            {
+                test:/\.js$/,
+                exclude:/node_modules/,
+                loader:"babel-loader"
+            }
+        ]
+    }
+
+}
